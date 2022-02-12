ac794030d8d2e6bd1fcd44f02e952355bc2ee0bd
Mark Vincent C. Pausanos <81241165+markpausanos@users.noreply.github.com>
2022-02-12 15:09:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const maintheme = document.getElementById("main_theme");
+
+function playMaintheme(){ //plays theme
+    maintheme.play();
+    maintheme.onended = function(){
+        playMaintheme();
+    }
+}
+playMaintheme(maintheme);
+
+function playAudio(x){
+    maintheme.play();
+    maintheme.onended = function(){
+        playMaintheme();
+    }
+}
+
+function playTransition(){
+    document.getElementById("open1").className = 'fadeOpen';
+    document.getElementById("open2").className = 'fadeOpen';
+    document.getElementById("mona").className = 'fadeMona';
+    document.getElementById("mona").style.visibility = "visible";
+}
